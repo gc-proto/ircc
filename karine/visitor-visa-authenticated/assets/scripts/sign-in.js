c598cbf3a26ad906c234bef5d5b2870cf4de8e25
bedardk <bedardk86@hotmail.com>
2023-09-19 15:26:15 -0400

    removed console log & fixed ID issue

--- karine/visitor-visa-authenticated/assets/scripts/sign-in.js
+++ karine/visitor-visa-authenticated/assets/scripts/sign-in.js
@@ -6,7 +6,7 @@
     const menu = document.querySelector('.menu-banner');
     const signin = document.querySelector('.signin-banner');
 
-    console.log(btnSignIn)
+    
     if(btnSignIn) {
         btnSignIn.onclick = function(e){
             e.preventDefault;
@@ -27,9 +27,7 @@
 
     $( document ).on( "wb-ready.wb", function( event ) {
 
-        console.log(localStorage.getItem('signedin'));
         if (localStorage.getItem('signedin') == "true") {
-            console.log('test');
             $.ajax({
                 url: "../../../assets/includes/sign-in.html",
                 type: "GET",
