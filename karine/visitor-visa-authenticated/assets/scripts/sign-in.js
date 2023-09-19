223a80d4dd3647da0cce636563de371de8f9606e
bedardk <bedardk86@hotmail.com>
2023-09-19 16:01:06 -0400

    Update sign-in.js

--- karine/visitor-visa-authenticated/assets/scripts/sign-in.js
+++ karine/visitor-visa-authenticated/assets/scripts/sign-in.js
@@ -1,8 +1,6 @@
-
+$( document ).on( "wb-ready.wb", function( event ) {
 
     const btnSignIn = document.getElementById('btn-sign-in');
-    const btnSignOut = document.getElementById('btn-sign-out');
-    console.log(btnSignOut);
     const menu = document.querySelector('.menu-banner');
     const signin = document.querySelector('.signin-banner');
 
@@ -15,7 +13,20 @@
             history.back();
             
         }
+    }    
+    if (localStorage.getItem('signedin') == "true") {
+        $.ajax({
+            url: "../../../assets/includes/sign-in.html",
+            type: "GET",
+            success: function(data){
+                $(signin).html(data);
+                menu.classList.add('hidden');
+            }
+        })
     }
+
+    const btnSignOut = document.getElementById('btn-sign-out');
+    console.log(btnSignOut);
     if(btnSignOut) {
         
         btnSignOut.onclick = function(e){
@@ -25,18 +36,5 @@
             location.reload();
         }
     }
-
-    $( document ).on( "wb-ready.wb", function( event ) {
-
-        if (localStorage.getItem('signedin') == "true") {
-            $.ajax({
-                url: "../../../assets/includes/sign-in.html",
-                type: "GET",
-                success: function(data){
-                    $(signin).html(data);
-                    menu.classList.add('hidden');
-                }
-            })
-        }
-    });
+});
     
