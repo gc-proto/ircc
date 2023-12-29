e3c4e6e8d975ad8db1cd94f51f5d8edbf00a08f3
bedardk <bedardk86@hotmail.com>
2023-12-29 11:23:10 -0500

    update hub

--- hub/d3/scripts/utils.js
+++
@@ -1,6 +0,0 @@
-//utility function to set attributes in short lines of code
-export function setAttributes(el, attrs) {
-    for (let key in attrs) {
-        el.setAttribute(key, attrs[key]);
-    }
-}
