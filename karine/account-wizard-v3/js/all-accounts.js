152079acc90c20b5bb7a798660809a2a00ff1356
bedardk <bedardk86@hotmail.com>
2024-07-12 10:53:41 -0400

    updates

--- karine/account-wizard-v3/js/all-accounts.js
+++ karine/account-wizard-v3/js/all-accounts.js
@@ -5,11 +5,35 @@ $(".account-chooser a").click(function (e) {
     }
 });
 
+
+
+                              
+
+
 $(".signin-create-btn").click(function (){
+    console.log("test")
+    document.querySelector('.account-banner').style.minHeight = "0px";
+    
+    $('.account-chooser').addClass('nojs-show');
+    $('.signin-create-btn').addClass('active');
 
-    let h = document.querySelector('.account-banner > .container').offsetHeight;
-    console.log(h)
+    let h;
 
-    document.querySelector('.account-banner').style.height = h + 100;
-    console.log(h)
+    if ($(this).hasClass('signin')) {
+        $('.signin-opt, .account-chooser').removeClass('nojs-show');
+        $('.find-header, .find-program').addClass('nojs-show');
+        $('.account-banner').addClass('sign-in').removeClass('create');        
+    }
+    else {
+        $('.find-header, .find-program, .account-chooser').removeClass('nojs-show');
+        $('.signin-opt').addClass('nojs-show');
+        $('.account-banner').addClass('create').removeClass('sign-in');
+    }
+
+    h = document.querySelector('.account-chooser').offsetHeight;
+    document.querySelector('.account-banner').style.minHeight = h + 100 + "px";
+      
+      
+   
+
 });
