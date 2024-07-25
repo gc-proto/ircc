ec625ae15fa1da18c391274a0c57b19f3e9d9d49
bedardk <bedardk86@hotmail.com>
2024-07-25 13:36:39 -0400

    Update all-accounts.js

--- js/all-accounts.js
+++ js/all-accounts.js
@@ -5,23 +5,22 @@ $(".account-chooser a").click(function (e) {
     }
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelectorAll('button[data-scroll-target]').forEach(function(button) {
-        button.addEventListener('click', function() {
-            var targetId = button.getAttribute('data-scroll-target');
-            var targetElement = document.getElementById(targetId);
 
-            if (targetElement) {
-                var offset = 20; // Adjust this value as needed to create space above the buttons
-                var elementPosition = targetElement.getBoundingClientRect().top;
-                var offsetPosition = elementPosition + window.pageYOffset - offset;
+document.querySelectorAll('button[data-scroll-target]').forEach(function(button) {
+    button.addEventListener('click', function() {
+        var targetId = button.getAttribute('data-scroll-target');
+        var targetElement = document.getElementById(targetId);
 
-                window.scrollTo({
-                    top: offsetPosition,
-                    behavior: 'smooth'
-                });
-            }
-        });
+        if (targetElement) {
+            var offset = 20; // Adjust this value as needed to create space above the buttons
+            var elementPosition = targetElement.getBoundingClientRect().top;
+            var offsetPosition = elementPosition + window.pageYOffset - offset;
+
+            window.scrollTo({
+                top: offsetPosition,
+                behavior: 'smooth'
+            });
+        }
     });
 });
 
