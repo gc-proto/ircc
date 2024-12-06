eaa22cd6b2d0eb134dfd63198528aad166605798
bedardk <bedardk86@hotmail.com>
2024-12-06 15:42:05 -0500

    icon counter

--- prototypes/franco-country-profiles/chart-v3.js
+++ prototypes/franco-country-profiles/chart-v3.js
@@ -113,19 +113,38 @@ c2 = new Chart(
 
 
 $(document).ready(function () {
-    
-    let iconCounter = document.querySelector('.icon-counter');
-    let activePopPercent = Math.round((parseInt(document.getElementById('pop-active-percent').innerHTML)/100)*10);
-    
-    console.log(activePopPercent)
+    let popActive = document.getElementById('pop-active');
+    let context = popActive.getContext('2d');
 
-    for (let i = 0; i < 10; i++) {
-        let icon = document.createElement('span');
-        icon.classList.add('fas', 'fa-user', 'mrgn-lft-sm');
-        if (i <= (activePopPercent-1) ){
-            icon.classList.add('active');
-        }
-        iconCounter.appendChild(icon);
+    // Function to resize canvas and draw the rectangle
+    function resizeCanvas() {
+        // Set canvas dimensions
+        popActive.width = popActive.parentElement.getBoundingClientRect().width; // 100% width
+        popActive.height = 65;              // Fixed height
+
+        // Draw background rectangle that fills the canvas
+        context.fillStyle = '#D8CDEC'; // Set rectangle color
+        context.fillRect(0, 0, popActive.width, popActive.height); // Draw rectangle
+
+        // Draw second rectangle (top bar)
+        let percentBar = parseFloat(document.getElementById('pop-active-percent').innerHTML) / 100;
+        const secondBarWidth = popActive.width * percentBar; // 70% of the full width
+        context.fillStyle = '#362950'; // Color for the second bar
+        context.fillRect(0, 0, secondBarWidth, 65); // Positioned over the first rectangle
+
+        const text = document.getElementById('pop-active-percent').innerHTML;
+        context.fillStyle = 'white'; // Text color
+        context.font = 'bold 24px Noto Sans'; // Font size and style
+        context.textAlign = 'left'; // left-align text
+        context.textBaseline = 'middle'; // Vertically center text
+        context.fillText(text + " %", 25, popActive.height / 2); // Position text in the middle of the second bar
     }
+
+    // // Initial canvas setup
+    resizeCanvas();
+
+
+    // Redraw the canvas on window resize
+    window.addEventListener('resize', resizeCanvas);
 
 });
