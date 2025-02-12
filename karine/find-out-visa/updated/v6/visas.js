0753543740261d673cb794f9610bc74fc5558372
bedardk <bedardk86@hotmail.com>
2025-02-12 16:13:33 -0500

    adding form validation

--- karine/find-out-visa/updated/v6/visas.js
+++ karine/find-out-visa/updated/v6/visas.js
@@ -5,6 +5,7 @@ document.getElementById('find-out-form').onsubmit = function (e) { e.preventDefa
 let data;
 let btnNext = document.getElementById("btn-next");
 let btnPrevious = document.getElementById("btn-previous");
+let form = document.getElementById("find-out-form");
 let userAnswers = [];
 
 let traveller_type, purpose_of_travel, travel_document, travel_type;
@@ -31,165 +32,177 @@ btnNext.onclick = function () {
         console.clear();
     }
     let currentQuestion = document.querySelector('.question:not(.hidden)');
+    if (!currentQuestion.querySelector('input:checked')) {
+        $(form).validate();
+        $(form).valid();
+    }
+    else {
 
-    if (currentQuestion.querySelector('#passport-code-selection') && (!document.getElementById('passport-code-selection').classList.contains('hidden'))) {
-        if (!document.getElementById('passport-code-selection').classList.contains('hidden')) {
-            traveller_type = (data["question-passport_code"][document.getElementById('passport-selection').getAttribute('data-passport-code')]).trim();
-            selectedInput = traveller_type;
+
+        if (currentQuestion.querySelector('#passport-code-selection') && (!document.getElementById('passport-code-selection').classList.contains('hidden'))) {
+            if (!document.getElementById('passport-code-selection').classList.contains('hidden')) {
+                traveller_type = (data["question-passport_code"][document.getElementById('passport-selection').getAttribute('data-passport-code')]).trim();
+                selectedInput = traveller_type;
+            }
         }
-    }
-    else {
-        selectedInput = currentQuestion.querySelector('input:checked').value;
-    }
+        else {
+            selectedInput = currentQuestion.querySelector('input:checked').value;
+        }
 
-    let question = currentQuestion.getAttribute('id');
-    let next;
+        let question = currentQuestion.getAttribute('id');
+        let next;
 
-    btnPrevious.classList.remove('hidden');
+        btnPrevious.classList.remove('hidden');
 
-    switch (question) {
-        case "question-traveller_type":
-            traveller_type = selectedInput;
-            next = document.getElementById(data[question][selectedInput]);
-            break;
+        switch (question) {
+            case "question-traveller_type":
+                traveller_type = selectedInput;
+                next = document.getElementById(data[question][selectedInput]);
+                break;
 
-        case "question-uspr":
-            if (selectedInput === "yes") {
-                traveller_type = "uspr"
-            }
-            else {
-                traveller_type = "unknown"
-            }
-            next = document.getElementById(data[question]);
-            break;
-        case "question-purpose_of_travel":
-            purpose_of_travel = selectedInput;
+            case "question-uspr":
+                if (selectedInput === "yes") {
+                    traveller_type = "uspr"
+                }
+                else {
+                    traveller_type = "unknown"
+                }
+                next = document.getElementById(data[question]);
+                break;
+            case "question-purpose_of_travel":
+                purpose_of_travel = selectedInput;
 
-            next = document.getElementById(data[question][traveller_type][selectedInput]);
-
-            break;
-        case "question-family":
-            purpose_of_travel = selectedInput;
-            next = document.getElementById(data[question][selectedInput][traveller_type]);
-            break;
-        case "question-study":
-        case "question-work":
-            console.log("passport_type: ", passport_type);
-            if (travel_type === "air" && userAnswers.includes(document.getElementById("question-travel"))) {
-                next = document.getElementById(data[question][traveller_type][travel_type][selectedInput]);
-            }
-            else if (passport_type === true) {
-                next = document.getElementById(data[question][traveller_type][passport_type][selectedInput]);
-            }
-            else {
                 next = document.getElementById(data[question][traveller_type][selectedInput]);
-            }
-            break;
-        case "question-travel":
-            travel_type = selectedInput;
-            if (!data[question][purpose_of_travel][traveller_type]) {
-                next = document.getElementById(data[question][purpose_of_travel]);
-            }
-            else {
-                next = document.getElementById(data[question][purpose_of_travel][traveller_type][selectedInput]);
-            }
-            break;
 
-        case "question-transit":
-            if ((traveller_type === "eTA-X" || traveller_type === "eTA-X-TWOV" || traveller_type === "mexico")) {
-                next = document.getElementById(data[question][traveller_type][travel_type][selectedInput]);
-            }
-            else {
+                break;
+            case "question-family":
+                purpose_of_travel = selectedInput;
+                next = document.getElementById(data[question][selectedInput][traveller_type]);
+                break;
+            case "question-study":
+            case "question-work":
+                console.log("passport_type: ", passport_type);
+                if (travel_type === "air" && userAnswers.includes(document.getElementById("question-travel"))) {
+                    next = document.getElementById(data[question][traveller_type][travel_type][selectedInput]);
+                }
+                else if (passport_type === true) {
+                    next = document.getElementById(data[question][traveller_type][passport_type][selectedInput]);
+                }
+                else {
+                    next = document.getElementById(data[question][traveller_type][selectedInput]);
+                }
+                break;
+            case "question-travel":
+                travel_type = selectedInput;
+                if (!data[question][purpose_of_travel][traveller_type]) {
+                    next = document.getElementById(data[question][purpose_of_travel]);
+                }
+                else {
+                    next = document.getElementById(data[question][purpose_of_travel][traveller_type][selectedInput]);
+                }
+                break;
+
+            case "question-transit":
+                if ((traveller_type === "eTA-X" || traveller_type === "eTA-X-TWOV" || traveller_type === "mexico")) {
+                    next = document.getElementById(data[question][traveller_type][travel_type][selectedInput]);
+                }
+                else {
+                    next = document.getElementById(data[question][traveller_type][selectedInput]);
+                }
+                break;
+            case "question-transit_length":
                 next = document.getElementById(data[question][traveller_type][selectedInput]);
-            }
-            break;
-        case "question-transit_length":
-            next = document.getElementById(data[question][traveller_type][selectedInput]);
-            break;
-        case "question-nonimmigrant_visa":
-            next = document.getElementById(data[question][traveller_type][purpose_of_travel][selectedInput]);
-            break;
+                break;
+            case "question-nonimmigrant_visa":
+                next = document.getElementById(data[question][traveller_type][purpose_of_travel][selectedInput]);
+                break;
 
-        case "question-travel_document":
-            traveller_type = selectedInput;
-            next = document.getElementById(data[question][purpose_of_travel][selectedInput]);
-            break;
-        case "question-travel_document_israel":
-        case "question-travel_document_romania":
-        case "question-travel_document_taiwan":
-            if (selectedInput === "yes") {
-                passport_type = true;
-            }
-            else {
-                passport_type = false;
-            }
-            if (!data[question][purpose_of_travel][selectedInput]) {
-                next = document.getElementById(data[question][purpose_of_travel]);
-            }
-            else {
+            case "question-travel_document":
+                traveller_type = selectedInput;
                 next = document.getElementById(data[question][purpose_of_travel][selectedInput]);
-            }
-            break;
+                break;
+            case "question-travel_document_israel":
+            case "question-travel_document_romania":
+            case "question-travel_document_taiwan":
+                if (selectedInput === "yes") {
+                    passport_type = true;
+                }
+                else {
+                    passport_type = false;
+                }
+                if (!data[question][purpose_of_travel][selectedInput]) {
+                    next = document.getElementById(data[question][purpose_of_travel]);
+                }
+                else {
+                    next = document.getElementById(data[question][purpose_of_travel][selectedInput]);
+                }
+                break;
 
-        case "question-passport_code":
+            case "question-passport_code":
 
 
-            if (traveller_type === "us_citizen" && purpose_of_travel === "family") {
-                next = document.getElementById(data["question-purpose_of_travel"][traveller_type]["family_notsupervisa"]);
-            }
-            else if (!data["question-purpose_of_travel"][traveller_type][purpose_of_travel]) {
-                next = document.getElementById(data["question-purpose_of_travel"][traveller_type]);
-            }
-            else {
-                next = document.getElementById(data["question-purpose_of_travel"][traveller_type][purpose_of_travel]);
-            }
-            break;
-        default:
-            if (!data[question][traveller_type][selectedInput]) {
-                next = document.getElementById(data[question][traveller_type]);
-            }
-            else {
-                next = document.getElementById(data[question][traveller_type][selectedInput]);
+                if (traveller_type === "us_citizen" && purpose_of_travel === "family") {
+                    next = document.getElementById(data["question-purpose_of_travel"][traveller_type]["family_notsupervisa"]);
+                }
+                else if (!data["question-purpose_of_travel"][traveller_type][purpose_of_travel]) {
+                    next = document.getElementById(data["question-purpose_of_travel"][traveller_type]);
+                }
+                else {
+                    next = document.getElementById(data["question-purpose_of_travel"][traveller_type][purpose_of_travel]);
+                }
+                break;
+            default:
+                if (!data[question][traveller_type][selectedInput]) {
+                    next = document.getElementById(data[question][traveller_type]);
+                }
+                else {
+                    next = document.getElementById(data[question][traveller_type][selectedInput]);
 
-            }
-            break;
+                }
+                break;
+        }
+
+        console.log("===================");
+        console.log("Question: " + question);
+        console.log("Selected input: ", selectedInput);
+        console.log("Traveller: ", traveller_type);
+        console.log("purpose_of_travel: ", purpose_of_travel);
+        console.log("Next: ", next);
+        console.log("===================");
+
+        userAnswers.push(currentQuestion);
+
+        if ((next.getAttribute("id") === "question-study" || next.getAttribute("id") === "question-work") && traveller_type === "us_citizen") {
+            document.getElementById('study-extend_permit').parentElement.classList.add('hidden');
+            document.getElementById('work-extend_permit').parentElement.classList.add('hidden');
+        }
+        else {
+            document.getElementById('study-extend_permit').parentElement.classList.remove('hidden');
+            document.getElementById('work-extend_permit').parentElement.classList.remove('hidden');
+        }
+
+        if (next.getAttribute('id').indexOf('question') > -1) {
+            console.log("go to a question");
+        }
+        else {
+            console.log("go to a result");
+            btnNext.classList.add('hidden');
+        }
+
+        currentQuestion.classList.add('hidden');
+        next.classList.remove('hidden');
     }
-
-    console.log("===================");
-    console.log("Question: " + question);
-    console.log("Selected input: ", selectedInput);
-    console.log("Traveller: ", traveller_type);
-    console.log("purpose_of_travel: ", purpose_of_travel);
-    console.log("Next: ", next);
-    console.log("===================");
-
-    userAnswers.push(currentQuestion);
-
-    if ((next.getAttribute("id") === "question-study" || next.getAttribute("id") === "question-work") && traveller_type === "us_citizen") {
-        document.getElementById('study-extend_permit').parentElement.classList.add('hidden');
-        document.getElementById('work-extend_permit').parentElement.classList.add('hidden');
-    }
-    else {
-        document.getElementById('study-extend_permit').parentElement.classList.remove('hidden');
-        document.getElementById('work-extend_permit').parentElement.classList.remove('hidden');
-    }
-
-    if (next.getAttribute('id').indexOf('question') > -1) {
-
-        console.log("go to a question");
-    }
-    else {
-        console.log("go to a result");
-        btnNext.classList.add('hidden');
-    }
-
-    currentQuestion.classList.add('hidden');
-    next.classList.remove('hidden');
 };
 
+
 btnPrevious.onclick = function () {
+    var validator = $(form).validate();
+    validator.resetForm();
+    if (document.getElementById('errors-'+form.getAttribute('id'))) document.getElementById('errors-'+form.getAttribute('id')).remove();
+
     let currentQuestion = document.querySelector('.question:not(.hidden)') ? document.querySelector('.question:not(.hidden)') : document.querySelector('.result:not(.hidden)');
     currentQuestion.classList.add('hidden');
+    if (currentQuestion.querySelector('input')) currentQuestion.querySelector('input').removeAttribute('required');
 
     if (currentQuestion.getAttribute("id") === 'question-passport_code' || currentQuestion.getAttribute('id') === 'question-travel_document') {
         traveller_type = "unknown";
